93e77291a158dee68a21a73ff169d5471f5684b8
data-miner00 <data-miner00@users.noreply.github.com>
2021-05-08 16:03:47 +0800

    fix: Disable background

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,10 +9,11 @@
 @import "~/assets/sass/layout"
 
 .root
-  background-image: url(~assets/images/background2.jpg)
-  background-size: cover
+  // background-image: url(~assets/images/background2.jpg)
+  // background-size: cover
   // background-attachment: fixed
   // background-repeat: no-repeat
-  background-position: center
+  // background-position: center
+  background: white
   height: 100vh
 </style>
