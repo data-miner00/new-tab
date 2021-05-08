d10df1b8b5495e023d5e047f70ef41ecc3941c11
data-miner00 <data-miner00@users.noreply.github.com>
2021-05-08 20:46:41 +0800

    feat: Added datetime

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="container">
     <div class="main">
+      <div class="time">
+        <div>
+          <img src="~/assets/images/day.png" alt="day" height="40" />
+        </div>
+        <div class="date-time">{{ date }} {{ time }}</div>
+      </div>
+
       <div class="greeting">Good morning Kyle. What would you like to do?</div>
     </div>
     <div class="side-section"></div>
@@ -14,6 +21,57 @@ export default Vue.extend({
   head: () => ({
     title: 'New Tab',
   }),
+  data: () => ({
+    date: '',
+    time: '',
+  }),
+  mounted() {
+    const dateObj: Date = new Date()
+
+    const _hour: number = dateObj.getHours()
+    const AMPM: string = _hour >= 12 ? 'PM' : 'AM'
+    const hour: number = _hour > 12 ? _hour - 12 : _hour
+    const minute: number = dateObj.getMinutes()
+    const time: string = `${hour}:${minute} ${AMPM}`
+
+    const month: string = this.months[dateObj.getMonth()]
+    const day: string = this.days[dateObj.getDay()]
+    const _date: number = dateObj.getDate()
+    const date: string = `${day}, ${month} ${_date}`
+
+    this.time = time
+    this.date = date
+  },
+
+  computed: {
+    months() {
+      return [
+        'January',
+        'Febrauary',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+      ]
+    },
+    days() {
+      return [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+      ]
+    },
+  },
 })
 </script>
 
@@ -33,9 +91,20 @@ export default Vue.extend({
 .greeting {
   font-size: 24px;
   font-weight: 600;
+  margin-top: 40px;
 }
 
 .side-section {
   background: #eee;
+}
+
+.time {
+  display: flex;
+  align-items: center;
+  color: gray;
+}
+
+.date-time {
+  margin-left: 10px;
 }
 </style>
