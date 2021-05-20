72fdac8b53436f2ae4ed643816c509c5c2d6920d
data-miner00 <data-miner00@users.noreply.github.com>
2021-05-20 13:08:26 +0800

    feat: Added navlinks

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,11 @@
   footer
     a.build-with(href="https://nuxtjs.org")
       img(src="~/assets/images/builtwithnuxt.png" height="20")
-    .sun
+    nav
+      a.link(href="#") About
+      a.link(href="#") Help
+      a.link(href="#") Contribute
+
       
 </template>
 
@@ -28,4 +32,16 @@ footer
 
   a
     display: block
+    text-decoration: none
+
+  nav
+    display: flex
+
+    .link
+      padding: 20px
+      color: #42b883
+      transition: color .2s
+
+      &:hover
+        color: #35495e
 </style>
