e964dd008bbb9681deaca0ea3519cef0fffebf77
data-miner00 <data-miner00@users.noreply.github.com>
2021-05-22 17:13:11 +0800

    feat: Added category link component

---
+++ components/Category.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  nuxt-link(to="/")
+    .category( :class="{'active': active}")
+      .chevron
+      .category-title {{ title }}
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    title: String,
+  },
+  computed: {
+    to() {
+      return '/web'
+    },
+    active() {
+      return this.title == 'Business'
+    },
+  },
+})
+</script>
+
+<style lang="sass" scoped>
+.category
+  display: flex
+  height: 28px
+  align-items: center
+  border-radius: 10px
+  padding: 30px 0
+  .category-title
+    text-transform: uppercase
+    font-weight: 500
+
+  &.active
+    background: #fafafa
+</style>
