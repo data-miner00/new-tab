f50fca0fedf925ed5bb33261bf0411c89c23f66a
data-miner00 <data-miner00@users.noreply.github.com>
2021-05-23 17:22:13 +0800

    feat: Added business page for web

---
+++ pages/web/business.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  .business
+    .showing Showing {{ current }} out of {{ total }} results.
+    .list-container
+      WebList(:sites="sites")
+      
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  //
+  layout: 'web',
+  data: () => ({
+    current: 10,
+    total: 25,
+    sites: [
+      {
+        name: 'Tesla',
+        description: 'This is lorem i[sum,',
+        tags: ['technology', 'aesthetic', 'electric'],
+        url: 'https://tesla.com',
+      },
+      {
+        name: 'Tesla',
+        description: 'This is lorem i[sum,',
+        tags: ['technology', 'aesthetic', 'electric'],
+        url: 'https://tesla.com',
+      },
+    ],
+  }),
+})
+</script>
+
+<style lang="sass" scoped>
+.showing
+  color: gray
+  font-size: .75em
+
+  .list
+</style>
