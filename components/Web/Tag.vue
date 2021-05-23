bfb1dd57d31ddd7905e451ed38591d60bb96f58a
data-miner00 <data-miner00@users.noreply.github.com>
2021-05-23 17:18:28 +0800

    feat: Added tag component

---
+++ components/Web/Tag.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .tag(:style="{background: randomColor}") {{ tag }}
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: ['tag'],
+  computed: {
+    randomColor() {
+      return '#' + Math.random().toString().slice(2, 8)
+    },
+  },
+})
+</script>
+
+<style lang="sass" scoped>
+.tag
+  border-radius: 25px
+  padding: 5px 8px
+  background: orange
+  font-size: .625em
+</style>
