3dca3875081158bafa7a41d2f638a834bbfa7cdc
data-miner00 <data-miner00@users.noreply.github.com>
2021-05-23 17:20:13 +0800

    feat: Added default page for web

---
+++ pages/web/index.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  .main
+    h1 What is this about?
+        
+    p 
+
+    //- table.list
+    //-   thead
+    //-     tr
+    //-       th Icon
+    //-       th Name
+    //-       th Snippet
+    //-       th Category
+    //-       th Tags
+    //-   tbody
+    //-     tr
+    //-       td Icon
+    //-       td Next.js
+    //-       td Lorem upsum solodr sit amet
+    //-       td 
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  //
+
+  layout: 'web',
+})
+</script>
+
+<style lang="sass" scoped></style>
