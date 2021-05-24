af568563ddbad316010115e8cd0f03d59caa4907
data-miner00 <data-miner00@users.noreply.github.com>
2021-05-24 20:34:33 +0800

    feat: Added notes page

---
+++ pages/notes/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="notes">
+    <client-only>
+      <Tiptap />
+    </client-only>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  //
+})
+</script>
+
+<style lang="sass" scoped>
+@import "../../assets/sass/_mixins"
+
+.notes
+  @include pageBaseStyle
+</style>
