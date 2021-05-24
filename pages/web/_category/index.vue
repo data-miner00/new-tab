10a9d403c74a5499288666311c84c896ecbf5547
data-miner00 <data-miner00@users.noreply.github.com>
2021-05-24 14:34:18 +0800

    chore: Refactored instance into dynamic page

--- pages/web/_category/index.vue
+++ pages/web/_category/index.vue
@@ -1,41 +1,58 @@
 <template lang="pug">
-  .business
-    .showing Showing {{ current }} out of {{ total }} results.
+  .category
+    .row
+      .showing Showing {{ current }} out of {{ total }} results. {{ category }}
+      .btn Create New
     .list-container
       WebList(:sites="sites")
-      
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+
 export default Vue.extend({
-  //
   layout: 'web',
   data: () => ({
-    current: 10,
-    total: 25,
-    sites: [
-      {
-        name: 'Tesla',
-        description: 'This is lorem i[sum,',
-        tags: ['technology', 'aesthetic', 'electric'],
-        url: 'https://tesla.com',
-      },
-      {
-        name: 'Tesla',
-        description: 'This is lorem i[sum,',
-        tags: ['technology', 'aesthetic', 'electric'],
-        url: 'https://tesla.com',
-      },
-    ],
+    current: 0,
+    total: 0,
+    sites: [{}],
   }),
+  async asyncData({ params }) {
+    const category = params.category
+    return { category }
+  },
+  mounted(): void {
+    this.current = 10
+    this.total = 25
+    this.sites = [
+      {
+        name: 'Tesla',
+        description: 'This is lorem i[sum,',
+        tags: ['technology', 'aesthetic', 'electric'],
+        url: 'https://tesla.com',
+      },
+      {
+        name: 'Tesla',
+        description: 'This is lorem i[sum,',
+        tags: ['technology', 'aesthetic', 'electric'],
+        url: 'https://tesla.com',
+      },
+    ]
+  },
 })
 </script>
 
 <style lang="sass" scoped>
-.showing
-  color: gray
+.row
+  display: flex
+  justify-content: space-between
+  align-items: center
   font-size: .75em
 
-  .list
+.showing
+  color: gray
+.btn
+  text-transform: uppercase
+  border: 1px solid #eee
+  padding: 5px 8px
 </style>
