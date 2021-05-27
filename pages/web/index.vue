24fd1d495d887ac39a6b7769b0368390db2d2ccf
data-miner00 <data-miner00@users.noreply.github.com>
2021-05-27 16:36:23 +0800

    feat: Simple landing for web management

--- pages/web/index.vue
+++ pages/web/index.vue
@@ -1,33 +1,69 @@
 <template lang="pug">
-  .main
-    h1 What is this about?
-        
-    p 
+  main.main
+    section.intro
+      h1 Web Repository
 
-    //- table.list
-    //-   thead
-    //-     tr
-    //-       th Icon
-    //-       th Name
-    //-       th Snippet
-    //-       th Category
-    //-       th Tags
-    //-   tbody
-    //-     tr
-    //-       td Icon
-    //-       td Next.js
-    //-       td Lorem upsum solodr sit amet
-    //-       td 
+      p
+        | The webpage can be categorized and labelled any a variety of ways.
+        | The webpage can be categorized and labelled any a variety of ways.
+        | The webpage can be categorized and labelled any a variety of ways.
 
+    section.usage
+      h1 Typical Usage
+
+      ul
+        li Add a new website
+        li View details of an existing website
+        li Edit an existing website
+        li Remove an existing website
+
+      h2 Add a website of interest
+      
+      
+    section.recomm
+      h1 Recommended
+
+      WebList(:sites="sites")
+    
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-  //
-
   layout: 'web',
+  data: () => ({
+    sites: [{}],
+  }),
+  mounted(): void {
+    this.sites = [
+      {
+        name: 'Tesla',
+        description: 'This is lorem i[sum,',
+        tags: ['technology', 'aesthetic', 'electric'],
+        url: 'https://tesla.com',
+      },
+      {
+        name: 'Tesla',
+        description: 'This is lorem i[sum,',
+        tags: ['technology', 'aesthetic', 'electric'],
+        url: 'https://tesla.com',
+      },
+    ]
+  },
 })
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+section
+  margin-bottom: 50px
+  & > *
+    margin-bottom: 20px
+
+
+  ul
+    padding: 20px 50px
+    background: #fafafa
+    display: flex
+    flex-direction: column
+    row-gap: 5px
+</style>
